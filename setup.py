769bdffc5fc9e92b3b4d00f76432822b5103b314
Swen Kühnlein <swen@dropastyle.com>
2017-02-17 23:41:43 +0100

    debianized broker

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tunneldigger-broker",
-    version="0.1",
+    version="0.2.0",
     packages=["tunneldigger"],
     package_dir={"tunneldigger": "broker"},
     entry_points={
@@ -11,4 +11,9 @@ setup(
             "tunneldigger = tunneldigger.main",
         ]
     },
+
+    author="wlan slovenija",
+    description="Client and broker for our custom L2TPv3 NAT-traversing tunnel setup protocol based on L2TPv3 support in the Linux kernel.",
+    license="AGPL",
+    url="http://tunneldigger.readthedocs.org/",
 )
